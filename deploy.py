b5167c5a8e96c46680d643b4d5b92db4705bc52f
Maahir Gupta <maahir.gupta@gmail.com>
2021-01-17 11:02:06 -0500

    Almost there

--- deploy.py
+++ deploy.py
@@ -1,3 +1,107 @@
+# from __future__ import absolute_import
+# from __future__ import division, print_function, unicode_literals
+
+# from sumy.parsers.html import HtmlParser
+# from sumy.parsers.plaintext import PlaintextParser
+# from sumy.nlp.tokenizers import Tokenizer
+# from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+# from sumy.nlp.stemmers import Stemmer
+# from sumy.utils import get_stop_words
+# from flask import Flask
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
 from fastpunct import FastPunct
 fastpunct = FastPunct('en')
-fastpunct.punct(["oh i thought you were here", "in theory everyone knows what a comma is", "hey how are you doing", "my name is sheela i am in love with hrithik"], batch_size=32)
+
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        self.initUI()
+
+    def button_clicked(self):
+        self.label.setText("Recording pressed the button")
+        self.update()
+
+        print(fastpunct.punct(["oh i thought you were here", "in theory everyone knows what a comma is", "hey how are you doing", "my name is sheela i am in love with hrithik"], batch_size=32))
+
+        LANGUAGE = "english"
+        SENTENCES_COUNT = 10
+
+        import speech_recognition as sr
+        for index, name in enumerate(sr.Microphone.list_microphone_names()):
+            print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        commentary = ""
+
+        while True:
+            with sr.Microphone(1) as source:
+                print("Say something!")
+                audio = r.listen(source)
+            # recognize speech using Google Speech Recognition
+            try:
+                speech = r.recognize_google(audio)
+                print("You said " + speech) # use default Google API key `r.recognize_google(audio, key = implied)`
+                commentary = commentary + speech # + ". "
+                if "Corpus" in speech:
+                    break
+            except sr.UnknownValueError:
+                print("Speech Recognition didn't catch that")
+            except sr.RequestError as e:
+                print("Could not request results from Speech Recognition service; {0}".format(e))
+            except KeyboardInterrupt:
+                break
+
+        print("PRE-PUNCTUATION:", commentary, "\n")
+        commentary = (fastpunct.punct([commentary], batch_size=32))
+        print("PUNCTUATED", commentary, "\n")
+
+        parser = PlaintextParser.from_string(commentary, Tokenizer(LANGUAGE))
+        stemmer = Stemmer(LANGUAGE)
+
+        summarizer = Summarizer(stemmer)
+        summarizer.stop_words = get_stop_words(LANGUAGE)
+
+        print("SUMMARIZED TEXT:")
+        for sentence in summarizer(parser.document, SENTENCES_COUNT):
+            print(sentence)
+
+    def initUI(self):
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Voice2Notes")
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Translate speech to summarized text!")
+        self.label.move(50,50)
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me!")
+        self.b1.clicked.connect(self.button_clicked)
+
+    def update(self):
+        self.label.adjustSize()
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+window()
